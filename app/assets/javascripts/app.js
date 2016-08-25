800a170486ae11cfe8b0706bc6ad2eddafb11ca5
max <hordijchuk.m.i@gmail.com>
2016-08-25 14:20:09 +0300

    Show dataset results separately. Remove 'capitalize' filter.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,7 +1,1 @@
 var app = angular.module("datasetsAnalysis", ["Devise"]);
-
-app.filter('capitalize', function() {
-    return function(input) {
-        return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
-    }
-});
